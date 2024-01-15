ebc4d7bb9c9b8d01c9a22f21dd0f79db0d20caa6
yureto <yut465p@gamil.com>
2024-01-15 20:33:32 +0900

    引数内のアノテーションでバリデーション実装

--- src/main/java/com/yureto/supergt/SuperGtController.java
+++ src/main/java/com/yureto/supergt/SuperGtController.java
@@ -1,5 +1,8 @@
 package com.yureto.supergt;
 
+import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +19,24 @@ public class SuperGtController {
     }
 
     @GetMapping("/superGt")
-    public List<SuperGt> findAll(@RequestParam(required = false) String driver) {
+    public ResponseEntity<List<SuperGt>> findAll(@RequestParam(required = false) String driver) {
+        List<SuperGt> result;
         if (driver != null) {
-            return superGtService.findByDriver(driver);
+            result = superGtService.findByDriver(driver);
         } else {
-            return superGtService.findAll();
+            result = superGtService.findAll();
         }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/superGt/{id}")
-    public SuperGt findById(@PathVariable("id") int id) {
-        return superGtService.findById(id);
+    public ResponseEntity<SuperGt> findById(@PathVariable("id") @Min(1) Integer id) {
+        SuperGt superGt = superGtService.findById(id);
+        if (superGt != null) {
+            return new ResponseEntity<>(superGt, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
