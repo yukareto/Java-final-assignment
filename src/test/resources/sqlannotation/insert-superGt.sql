ab7ea68507d8eaa2f0b1fb3ea1609aa3c19736d4
yureto <yut465p@gamil.com>
2024-05-25 17:41:20 +0900

    findAllのテストコードを実装

---
+++ src/test/resources/sqlannotation/insert-superGt.sql
@@ -0,0 +1,7 @@
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (1, "山本尚貴", "RAYBRIG NSX-GT", 100);
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (2, "大湯都史樹", "ARTA MUGEN NSX-GT", 8);
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (3, "立川裕路", "ZENT CERUMO GR Supra", 38);
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (4, "蒲生尚弥", "LEON PYRAMID AMG", 65);
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (5, "井口卓人", "SUBARU BRZ R&D SPORT", 61);
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (6, "荒聖治", "Studie BMW M4", 7);
+INSERT INTO superGt (id, driver, affiliated_team, car_number) VALUES (7, "谷口信輝", "GOODSMILE RACING & TeamUKYO", 4);
