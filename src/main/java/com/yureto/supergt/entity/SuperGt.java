552d4d432799c74ed3bbfca29902928f5e643b80
yureto <yut465p@gamil.com>
2024-05-26 09:11:32 +0900

    entityの内容を1度削除

--- src/main/java/com/yureto/supergt/entity/SuperGt.java
+++ src/main/java/com/yureto/supergt/entity/SuperGt.java
@@ -1,7 +1,5 @@
 package com.yureto.supergt.entity;
 
-import java.util.Objects;
-
 public class SuperGt {
     public Integer id;
     public String driver;
@@ -45,18 +43,5 @@ public class SuperGt {
 
     public void setCarNumber(String carNumber) {
         this.carNumber = carNumber;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SuperGt superGt = (SuperGt) o;
-        return Objects.equals(id, superGt.id) && Objects.equals(driver, superGt.driver) && Objects.equals(affiliatedTeam, superGt.affiliatedTeam) && Objects.equals(carNumber, superGt.carNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, driver, affiliatedTeam, carNumber);
     }
 }
