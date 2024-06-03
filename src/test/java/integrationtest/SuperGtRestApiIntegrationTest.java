0832d85cfd8bcdd850b258e6284368d3fed6069c
yureto <yut465p@gamil.com>
2024-06-03 20:10:50 +0900

    結合テストのコードにテキストブロックを使用した実装に変更

--- src/test/java/integrationtest/SuperGtRestApiIntegrationTest.java
+++ src/test/java/integrationtest/SuperGtRestApiIntegrationTest.java
@@ -34,49 +34,53 @@ public class SuperGtRestApiIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals("[" +
-                " {" +
-                " \"id\": 1," +
-                " \"driver\": \"山本尚貴\"," +
-                " \"affiliatedTeam\": \"RAYBRIG NSX-GT\"," +
-                " \"carNumber\": \"100\"" +
-                " }," +
-                " {" +
-                " \"id\": 2," +
-                " \"driver\": \"大湯都史樹\"," +
-                " \"affiliatedTeam\": \"ARTA MUGEN NSX-GT\"," +
-                " \"carNumber\": \"8\"" +
-                " }," +
-                " {" +
-                " \"id\": 3," +
-                " \"driver\": \"立川裕路\"," +
-                " \"affiliatedTeam\": \"ZENT CERUMO GR Supra\"," +
-                " \"carNumber\": \"38\"" +
-                " }," +
-                " {" +
-                " \"id\": 4," +
-                " \"driver\": \"蒲生尚弥\"," +
-                " \"affiliatedTeam\": \"LEON PYRAMID AMG\"," +
-                " \"carNumber\": \"65\"" +
-                " }," +
-                " {" +
-                " \"id\": 5," +
-                " \"driver\": \"井口卓人\"," +
-                " \"affiliatedTeam\": \"SUBARU BRZ R&D SPORT\"," +
-                " \"carNumber\": \"61\"" +
-                " }," +
-                " {" +
-                " \"id\": 6," +
-                " \"driver\": \"荒聖治\"," +
-                " \"affiliatedTeam\": \"Studie BMW M4\"," +
-                " \"carNumber\": \"7\"" +
-                " }," +
-                " {" +
-                " \"id\": 7," +
-                " \"driver\": \"谷口信輝\"," +
-                " \"affiliatedTeam\": \"GOODSMILE RACING & TeamUKYO\"," +
-                " \"carNumber\": \"4\"" +
-                " }" +
-                "]", response, JSONCompareMode.STRICT);
+        String expectedResponse = """
+            [
+                {
+                    "id": 1,
+                    "driver": "山本尚貴",
+                    "affiliatedTeam": "RAYBRIG NSX-GT",
+                    "carNumber": "100"
+                },
+                {
+                    "id": 2,
+                    "driver": "大湯都史樹",
+                    "affiliatedTeam": "ARTA MUGEN NSX-GT",
+                    "carNumber": "8"
+                },
+                {
+                    "id": 3,
+                    "driver": "立川裕路",
+                    "affiliatedTeam": "ZENT CERUMO GR Supra",
+                    "carNumber": "38"
+                },
+                {
+                    "id": 4,
+                    "driver": "蒲生尚弥",
+                    "affiliatedTeam": "LEON PYRAMID AMG",
+                    "carNumber": "65"
+                },
+                {
+                    "id": 5,
+                    "driver": "井口卓人",
+                    "affiliatedTeam": "SUBARU BRZ R&D SPORT",
+                    "carNumber": "61"
+                },
+                {
+                    "id": 6,
+                    "driver": "荒聖治",
+                    "affiliatedTeam": "Studie BMW M4",
+                    "carNumber": "7"
+                },
+                {
+                    "id": 7,
+                    "driver": "谷口信輝",
+                    "affiliatedTeam": "GOODSMILE RACING & TeamUKYO",
+                    "carNumber": "4"
+                }
+            ]
+        """;
+
+        JSONAssert.assertEquals(expectedResponse, response, JSONCompareMode.STRICT);
     }
 }
